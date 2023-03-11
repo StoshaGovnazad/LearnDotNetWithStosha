7eb89be5298e765d202b8d73503570c8d1731f42
Stosha Govnazad <JiruhHusein@gmail.com>
2023-03-11 13:26:31 +0300

    New class which says Hello

---
+++ Module1/Module1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Module1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string Hello = Console.ReadLine();
+
+            Hello += ", привет!";
+
+            Console.WriteLine(Hello);
+        }
+    }
+}
